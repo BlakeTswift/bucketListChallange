1cd1451a4e78ea712ce3a887504f2c09d48c77ac
BlakeT <BlakeTswift@gmail.com>
2025-10-21 14:35:52 -0600

    Initial Commit

---
+++ bucketListChallange/bucketListChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bucketListChallangeApp.swift
+//  bucketListChallange
+//
+//  Created by Trytten, Blake - Student on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct bucketListChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bucketListChallangeTests/bucketListChallangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  bucketListChallangeTests.swift
+//  bucketListChallangeTests
+//
+//  Created by Trytten, Blake - Student on 10/21/25.
+//
+
+import Testing
+@testable import bucketListChallange
+
+struct bucketListChallangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
